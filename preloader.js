d9e79dc1996132226a7424619f79676f388b8b69
Titoneli <titoneli@me.com>
2025-06-04 16:27:05 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://Titoneli.github.io/preloaderJs/cp_loading.svg';
+svgImage.src = 'https://Titoneli.github.io/preloaderJs/preload_1click.png';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
